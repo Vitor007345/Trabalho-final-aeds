c914e1116c3f59018ffb169055e4e5836752e30a
Vítor Albuquerque Dinelli <vitordinelli2007@hotmail.com>
2025-06-26 08:17:30 -0300

    feat: função setCpf e setNome

--- V1.0/ProjetoFinal/person.cpp
+++ V1.0/ProjetoFinal/person.cpp
@@ -1,11 +1,28 @@
 #include <string>
+#include <regex>
 #include "person.hpp"
+
+using namespace std;
 
 
 int Person::qntOfInstances = 0;
 
 
-std::string Person::getCpf() const{return this->cpf;}
-std::string Person::getName() const{return this->name;}
+string Person::getCpf() const{return this->cpf;}
+string Person::getName() const{return this->name;}
 const Date& Person::getBirth() const{return *(this->birth);};
 int Person::getQntOfInstances() {return Person::qntOfInstances;}
+
+bool Person::setCpf(const string& cpf){
+    bool success = false;
+    static const regex cpfToken(R"(\d{3}\.\d{3}\.\d{3}-\d{2})");
+    if(regex_match(cpf, cpfToken)){
+        this->cpf = cpf;
+        success = true;
+    }
+    return success;
+}
+bool Person::setName(const string& name){
+    this->name = name;
+    return true;
+}
